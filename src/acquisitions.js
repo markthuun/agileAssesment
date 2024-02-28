c1d6c3db31c74cc8f880e69ab5bc2c076b4daa0a
Mark <mark@thuun.com>
2024-02-28 10:24:24 +0100

    Update acquisitions.js

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,29 +1,51 @@
 import Chart from 'chart.js/auto'
 
 (async function() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+const data = {
+  labels: [
+    'Agile',
+    'Agile1',
+    'Agile2',
+    'Agile3',
+    'Agile4',
+    'Agile5',
+
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [5,2,3,1,2,3],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }, {
+    label: 'My Second Dataset',
+    data: [2,2,1,4,2,3],
+    fill: true,
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    pointBackgroundColor: 'rgb(54, 162, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
+  }]
+};
 
   new Chart(
     document.getElementById('acquisitions'),
     {
-      type: 'bar',
-      data: {
-        labels: data.map(row => row.year),
-        datasets: [
-          {
-            label: 'Acquisitions by year',
-            data: data.map(row => row.count)
+      type: 'radar',
+        data: data,
+        options: {
+          elements: {
+            line: {
+              borderWidth: 3
+            }
           }
-        ]
-      }
+      },
     }
   );
 })();
